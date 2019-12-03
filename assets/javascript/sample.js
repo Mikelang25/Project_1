749ca52335ffbbaff121a47544618e5dc896e5a7
Douglas Mueller <douglasmueller6@gmail.com>
2019-12-02 20:58:42 -0500

    london time and firebase authorization

--- assets/javascript/sample.js
+++ assets/javascript/sample.js
@@ -1,9 +1,10 @@
-var premierTeams = [];
+
 
 
 $(document).ready(function() {
-    var current;
+var current;
 var currentTime;
+
 
 // Firebase configuration
 var firebaseConfig = {
@@ -22,8 +23,9 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 var database = firebase.database();
 var auth = firebase.auth();
-//listen for auth changes (user login/logout)
-auth.onAuthStateChanged(user => {
+
+ //listen for auth changes (user login/logout)
+ auth.onAuthStateChanged(user => {
     if (user) {
         console.log(user);
         $("#currentUserDetails").empty();
@@ -36,16 +38,9 @@ auth.onAuthStateChanged(user => {
 
 });
 
-   //current London date and time function shown in html
-   function currentTime() {
-    var current = moment().tz('Europe/London').format('MMMM Do YYYY, h:mm:ss A');
-    $("#time").html("London time: " + current);
-    setTimeout(currentTime, 1000);
-};
-    currentTime();
 
-    // Calls the sportDB api to get the upcoming 15 premier league matches 
     var queryURL = "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328"
+
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -60,20 +55,12 @@ auth.onAuthStateChanged(user => {
            $("#upcoming-games").append(newGame);
         }
     });
-
-
-
-
-    //Calls the sportDB api to get all the teams in the premier league 
-    var queryURL2 = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League"
-    $.ajax({
-        url: queryURL2,
-        method: "GET"
-    }) .then(function(response2) {
-        for(var j=0;j<20;j++){
-            premierTeams.push(response2.teams[j].strTeam);
-        }
-        console.log(premierTeams);
-    });
+    //current London date and time function shown in html
+function currentTime() {
+    var current = moment().tz('Europe/London').format('MMMM Do YYYY, h:mm:ss A');
+    $("#time").html("London time: " + current);
+    setTimeout(currentTime, 1000);
+};
+    currentTime();
 
 });
